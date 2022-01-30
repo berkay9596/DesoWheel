1ffca749efa2773e678890560a74fc761939d9d5
Berkay <berkaayyuksel@gmail.com>
2022-01-30 03:57:10 +0300

    responsive

--- src/Layouts/HomePage.js
+++ src/Layouts/HomePage.js
@@ -53,7 +53,9 @@ const HomePage = () => {
           </div>
         </div>
       </div>
-      <WaveEffect />
+      <div style={{ marginTop: "5em" }}>
+        <WaveEffect />
+      </div>
     </>
   );
 };
