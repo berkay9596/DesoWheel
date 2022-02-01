58598c50c121c9dc9201fe045eac060de82d13de
Berkay9596 <53509972+berkay9596@users.noreply.github.com>
2022-02-01 13:17:45 +0300

    not authorized for posts page

--- src/Layouts/Posts.js
+++ src/Layouts/Posts.js
@@ -16,19 +16,23 @@ const Posts = () => {
     }, [public_key])
 
     return (
-        <div className="container d-flex align-items-center justify-content-center" style={{ height: "100vh", flexWrap: "wrap", marginTop: "5rem" }}>
+        <div className="container d-flex justify-content-center align-items-center" style={{ height: "100vh" }}>  {public_key ? < div className="container d-flex align-items-center justify-content-center" style={{ height: "100vh", flexWrap: "wrap", marginTop: "5rem" }
+        }>
             <h2>You can simply click on the buttons to make a draw among the post that you clicked.</h2>
-            {state?.info?.UserPosts?.Posts?.map((post, index) =>
-                <div key={index}>
-                    <div className="card" style={{ width: "60vw" }}>
-                        <div className="card-body">
-                            <img src={state?.info?.ProfilePicture} alt="Profile" style={{ width: "40px", borderRadius: "20px" }} /><span style={{ fontWeight: "bold" }}> {state?.info?.UserName}</span>
-                            <p className="card-text">{post?.Body}</p>
-                            <a href="/posts" className="btn btn-primary mx-1">Reposts ({post?.RecloutCount}) </a>
-                            <a href="/posts" className="btn btn-danger">Likes  ({post?.LikeCount})</a>
+            {
+                state?.info?.UserPosts?.Posts?.map((post, index) =>
+                    <div key={index}>
+                        <div className="card" style={{ width: "60vw" }}>
+                            <div className="card-body">
+                                <img src={state?.info?.ProfilePicture} alt="Profile" style={{ width: "40px", borderRadius: "20px" }} /><span style={{ fontWeight: "bold" }}> {state?.info?.UserName}</span>
+                                <p className="card-text">{post?.Body}</p>
+                                <a href="/posts" className="btn btn-primary mx-1">Reposts ({post?.RecloutCount}) </a>
+                                <a href="/posts" className="btn btn-danger">Likes  ({post?.LikeCount})</a>
+                            </div>
                         </div>
-                    </div>
-                </div>)}
+                    </div>)
+            }
+        </div > : <h1> Sign in to see your posts here..</h1>}
         </div>
     )
 }
