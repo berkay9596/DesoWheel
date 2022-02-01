3e3eaa29c445ed648220014410d93ea7b25af7d5
Berkay9596 <53509972+berkay9596@users.noreply.github.com>
2022-02-01 16:43:25 +0300

    loading for posts page

--- src/Layouts/Posts.js
+++ src/Layouts/Posts.js
@@ -1,38 +1,59 @@
 import { useSelector, useDispatch } from "react-redux";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getInfo } from '../redux/actions/userInfoActions'
+import { TailSpin } from "react-loader-spinner";
 
 const Posts = () => {
     const dispatch = useDispatch();
+    const [loading, setLoading] = useState(true);
     const state = useSelector(state => state.info)
     const public_key = localStorage.getItem('publicKey');
     const headers = {
         public_key: public_key,
     };
-    console.log("state", state)
     useEffect(() => {
         dispatch(getInfo(headers))
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [public_key])
 
+    useEffect(() => {
+        if (state?.info?.UserPosts?.Posts) {
+            setLoading(false)
+        }
+        if (!state?.info?.UserPosts?.Posts) {
+            return setLoading(true)
+        }
+    }, [state])
     return (
-        <div className="container d-flex justify-content-center align-items-center" style={{ height: "100vh" }}>  {public_key ? < div className="container d-flex align-items-center justify-content-center" style={{ height: "100vh", flexWrap: "wrap", marginTop: "5rem" }
-        }>
-            <h2>You can simply click on the buttons to make a draw among the post that you clicked.</h2>
-            {
-                state?.info?.UserPosts?.Posts?.map((post, index) =>
-                    <div key={index}>
-                        <div className="card" style={{ width: "60vw" }}>
-                            <div className="card-body">
-                                <img src={state?.info?.ProfilePicture} alt="Profile" style={{ width: "40px", borderRadius: "20px" }} /><span style={{ fontWeight: "bold" }}> {state?.info?.UserName}</span>
-                                <p className="card-text">{post?.Body}</p>
-                                <a href="/posts" className="btn btn-primary mx-1">Reposts ({post?.RecloutCount}) </a>
-                                <a href="/posts" className="btn btn-danger">Likes  ({post?.LikeCount})</a>
+        <div className="container d-flex justify-content-center align-items-center" style={{ height: "100vh" }}>
+            < div className="container d-flex align-items-center justify-content-center" style={{ height: "100vh", flexWrap: "wrap", marginTop: "5rem" }}>
+                {loading ? <div
+                    style={{
+                        position: "absolute",
+                        top: "50%",
+                        left: "50%",
+                        transform: "translate(-50%,-50%)",
+                    }}
+                >
+                    <TailSpin heigth="100" width="100" color="red" ariaLabel="loading" />
+                </div> : <>
+                    <h2>You can simply click on the buttons to make a draw among the post that you clicked.</h2>
+                    {state?.info?.UserPosts?.Posts?.map((post, index) =>
+                        <div key={index}>
+                            <div className="card" style={{ width: "60vw" }}>
+                                <div className="card-body">
+                                    <img src={state?.info?.ProfilePicture} alt="Profile" style={{ width: "40px", borderRadius: "20px" }} /><span style={{ fontWeight: "bold" }}> {state?.info?.UserName}</span>
+                                    <p className="card-text">{post?.Body}</p>
+                                    <a href="/posts" className="btn btn-primary mx-1">Reposts ({post?.RecloutCount}) </a>
+                                    <a href="/posts" className="btn btn-danger">Likes  ({post?.LikeCount})</a>
+                                </div>
                             </div>
                         </div>
-                    </div>)
-            }
-        </div > : <h1> Sign in to see your posts here..</h1>}
+                    )}
+
+                </>
+                }
+            </div>
         </div>
     )
 }
